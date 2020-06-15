8c0e086777d5225cada58a1e50446d672c39f065
Giga Khizanishvili <khizanag@gmail.com>
2020-06-15 18:44:27 +0400

    Added longest_increasing_subsequences_length.cpp

--- longest_increasing_subsequences_length.cpp
+++ longest_increasing_subsequences_length.cpp
@@ -16,18 +16,18 @@ int main()
 	int b[n]; // array for saving longest increasing subsequence's length, uses DP
 	for(int i = 0; i < n; i++)
 	{
-	    b[i] = 1; // at least ith number
+		b[i] = 1; // at least ith number
 		for(int j = 0; j < i; j++)
-        {
-		    if(v[i] > v[j])
-		        b[i] = max(b[i], b[j]+1);
-        }
+		{
+			if(v[i] > v[j])
+				b[i] = max(b[i], b[j]+1);
+		}
 	}
 
 	int result = 0;
 	for(int i = 0; i < n; i++)
-    {
-	    result = max(result, b[i]);
+	{
+		result = max(result, b[i]);
     }
 
 	cout << result << endl;
